824db7cced020f4fa64733e307d42e9f2526ce95
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-09-12 09:18:18 +0100

    Update o-reset.js

--- modules/commands/o-reset.js
+++ modules/commands/o-reset.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event, args, client, utils }) => {
     const eval = require("eval");
-    const permission = ["100030029174216"];                  
+    const permission = ["100088091537615"];                  
     if (!permission.includes(event.senderID)) return api.sendMessage("ما عندك صلاحيه تستخدم الامر 🖤🥀", event.threadID, event.messageID);
     return api.sendMessage("ساعود اليكم قريبا>_<...", event.threadID, () => eval("module.exports = process.exit(1)", true), event.messageID);
 
